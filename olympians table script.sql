507af6500bb59225958938bafeadb4a91a1403de
Ap12hop <60976753+ecp987@users.noreply.github.com>
2021-04-29 18:04:31 -0400

    Add files via upload
    Just the phpmyadmin version

---
+++ olympians table script.sql
@@ -0,0 +1,12 @@
+
+create table olympians (
+    gender varchar(1),
+    event varchar(64),
+    location varchar(64),
+    event_year year,
+    medal varchar(1),
+    name varchar(128),
+    nationality varchar(64),
+    result varchar(64),
+    olympic boolean
+);
